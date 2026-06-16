892942ce1bad1afa771ac7a1cb76ecd7a203936d
clarke-f <clarkefred61@gmail.com>
2026-06-16 20:11:46 +0100

    Moved calculateFinalPrice method to product

--- src/main/java/com/codemanship/refactoring/featureenvy/Product.java
+++ src/main/java/com/codemanship/refactoring/featureenvy/Product.java
@@ -16,4 +16,14 @@ class Product {
     public boolean isOnSale() {
         return onSale;
     }
+
+    public double calculateFinalPrice() {
+        double price = getPrice();
+
+        if (isOnSale()) {
+            price = price * 0.8;
+        }
+
+        return price;
+    }
 }
